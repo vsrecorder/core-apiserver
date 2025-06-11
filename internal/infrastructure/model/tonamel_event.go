1a57f649dab2ecde0e5ccdba5b62cdc522d7fab2
sosomasox <sosomasox@gmail.com>
2025-06-11 10:52:31 +0900

    TonamelEventに関する処理の修正

--- internal/infrastructure/model/tonamel_event.go
+++ internal/infrastructure/model/tonamel_event.go
@@ -1,13 +1,18 @@
 package model
 
-type ResData struct {
-	Result      bool   `json:"result"`
+type Metadata struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Image       string `json:"og:image"`
 }
 
+type Result struct {
+	URL      string `json:"url"`
+	Success  bool   `json:"success"`
+	Metadata `json:"metadata"`
+}
+
 type TonamelEvent struct {
-	Result  bool `json:"result"`
-	ResData `json:"resData"`
+	Success bool `json:"success"`
+	Result  `json:"result"`
 }
